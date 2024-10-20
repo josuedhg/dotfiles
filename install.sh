9c0ac7f072f8e9502f83e73fe3d7a80128e4e70a
Josue David Hernandez Gutierrez <megadavidtor@gmail.com>
2024-10-20 11:57:32 -0600

    feat(config/nvim): add nvim configuration
    Signed-off-by: Josue David Hernandez Gutierrez <megadavidtor@gmail.com>

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/bash
 
-#ln -sf $PWD/nvim ~/.config/nvim
+CONFIG_FOLDER="$PWD/config/"
+TARGET_CONFIG_FOLDER="$HOME/.config/"
+
+if [ ! -d "$TARGET_CONFIG_FOLDER" ]; then
+	mkdir -p $TARGET_CONFIG_FOLDER
+fi
+for folder in $(ls $CONFIG_FOLDER); do
+	if [ ! -d "$TARGET_CONFIG_FOLDER/$folder" ]; then
+		ln -sf $CONFIG_FOLDER/$folder $TARGET_CONFIG_FOLDER/$folder
+	fi
+done
